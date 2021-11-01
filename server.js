a2b9db6028366e473bb6efe927a358e2e9c81bdc
alba02 <naomimaldo02@gmail.com>
2021-11-01 16:37:18 -0400

    Create web server

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ const server = http.createServer((request, response) => {
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
-
+/*
 console.log(__filename);
 console.log(__dirname);
+
+const {readFile, readFileSync}= require('fs');
+
+try{
+  const data = readFileSync("hi.text", "utf-8");
+console.log(data);
+} catch(err){
+  console.error(err);
+}
+fs.readfileSync('hi.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log("Log from outside");
