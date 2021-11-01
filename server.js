a79f0596fef1a3a98e0c9dc283f2cffade0977ca
alba02 <naomimaldo02@gmail.com>
2021-11-01 16:49:57 -0400

    Add hi.text file and read file

--- server.js
+++ server.js
@@ -15,17 +15,13 @@ server.listen(PORT, HOSTNAME, () => {
 
 console.log(__filename);
 console.log(__dirname);
-/*
-const {readFile, readFileSync}= require('fs');
 
-try{
-  const data = readFileSync("hi.text", "utf-8");
-console.log(data);
-} catch(err){
+const { readFile, readFileSync } = require('fs');
+
+try {
+  const data = readFileSync('hi.text', 'utf-8');
+  console.log(data);
+} catch (err) {
   console.error(err);
 }
-fs.readfileSync('hi.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log("Log from outside");
+console.log('Log from outside');
