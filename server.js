537f487abe03ee3162dc0d835ce05e1044538e68
alba02 <naomimaldo02@gmail.com>
2021-11-02 01:45:57 -0400

    Move status code to each case

--- server.js
+++ server.js
@@ -5,19 +5,21 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
 
 let path = "./";
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html"
+      res.statusCode = 200;
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
